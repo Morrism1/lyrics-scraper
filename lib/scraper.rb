77db568579bda476e615ae34cdf1a50fe3a5905b
Morrism1 <morrismontana0@gmail.com>
2020-07-13 14:33:03 +0200

    Add methods to scrape the page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,22 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'open-uri'
 
 class Scraper
+  def initialize(artist, song)
+    @artist = artist
+    @song = song
+    @url = url
+  end
 
+  def scrape
+    doc = Nokogiri::HTML(URI.open(url_maker(@artist, @song)))
+    doc.css('body > div.container.main-page > div > div.col-xs-12.col-lg-8.text-center >
+            div:nth-child(8)').each { |link| puts link.content }
+  end
+
+  def url_maker(artist, song)
+    "https://www.azlyrics.com/lyrics/#{artist}/#{song}.html"
+  end
 end
