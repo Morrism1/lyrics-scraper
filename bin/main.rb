eeb61b30c300e108d92172aa31d00ad625f9792a
Morrism1 <morrismontana0@gmail.com>
2020-07-21 13:09:15 +0200

    Add a line showing new loop session and remove uncesserary codes

--- bin/main.rb
+++ bin/main.rb
@@ -4,28 +4,21 @@ artist = ''
 song = ''
 
 loop do
-  puts 'Enter Name of the artist or Exit or Quit to abort'
+  puts 'Enter Name of the ARTIST or Exit or Quit to abort'
   artist = gets.chomp
-  if artist.empty? || artist == ' '
-    puts 'Please, do not leave artist input empty, Enter the Name of Artist'
-    artist = gets.chomp
-  end
 
   abort if artist =~ /(?:ex|qu)it/i
 
-  puts 'Enter Name of the Song or Exit to quit'
+  puts 'Enter Name of the SONG or Exit to quit'
 
   song = gets.chomp
-  if song.empty?
-    puts 'Please, do not leave song input empty, Enter the Name of the Song'
-    song = gets.chomp
-  end
 
   abort if song =~ /(?:ex|qu)it/i
 
   break if !artist.empty? && !song.empty?
 
-  puts 'Provide name for song and artist to quit the loop'
+  puts "Provide name for SONG and ARTIST to quit the loop or type exit\r\n"
+  puts '-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-'
 end
 
 puts '----------------------------------------------'
