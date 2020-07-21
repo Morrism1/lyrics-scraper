86fdf719da565046e85107e3182d3cb1c64b56dc
Morrism1 <morrismontana0@gmail.com>
2020-07-21 12:57:04 +0200

    Add option to quit loop and tell user not leave input empty

--- bin/main.rb
+++ bin/main.rb
@@ -4,14 +4,28 @@ artist = ''
 song = ''
 
 loop do
-  puts 'Enter Name of the artist'
+  puts 'Enter Name of the artist or Exit or Quit to abort'
   artist = gets.chomp
+  if artist.empty? || artist == ' '
+    puts 'Please, do not leave artist input empty, Enter the Name of Artist'
+    artist = gets.chomp
+  end
 
-  puts 'Enter Name of the Song'
+  abort if artist =~ /(?:ex|qu)it/i
+
+  puts 'Enter Name of the Song or Exit to quit'
 
   song = gets.chomp
+  if song.empty?
+    puts 'Please, do not leave song input empty, Enter the Name of the Song'
+    song = gets.chomp
+  end
+
+  abort if song =~ /(?:ex|qu)it/i
 
   break if !artist.empty? && !song.empty?
+
+  puts 'Provide name for song and artist to quit the loop'
 end
 
 puts '----------------------------------------------'
